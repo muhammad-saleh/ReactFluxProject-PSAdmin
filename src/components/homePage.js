2c26c74906b4cf5f40bfe94f6a181331c5d95f83
Mohamed Saleh <eng.m.ahmed@live.com>
2015-12-24 01:34:23 +0200

    Creating first component which is home page

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function(){
+        return (
+            <div className="jumbotron">
+                <h1>PluralSight Administration</h1>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
